bf45f5f9fc747e0525df7d87e2437eb0f2bccf46
Marcelina Woziwoda <mwoziwod@akamai.com>
2026-03-02 20:13:42 +0100

    feat(EDNS): New module for EDNS

---
+++ new-edns/versions.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    akamai = {
+      source  = "akamai/akamai"
+      version = "~> 9.2"
+    }
+
+    time = {
+      source = "hashicorp/time"
+      version = "~> 0.13"
+    }
+    dns = {
+      source  = "hashicorp/dns"
+      version = "~> 3.4"
+    }
+  }
+  required_version = ">= 1.9.0"
+
+}
