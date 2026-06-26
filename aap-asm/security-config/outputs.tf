bddd1cd3e05f4df4f647bc32d93a7ef6af322cf5
spratap <spratap@gmail.com>
2026-06-26 20:16:27 +0530

    style(aapasm): fix terraform formatting in security-config outputs

--- aap-asm/security-config/outputs.tf
+++ aap-asm/security-config/outputs.tf
@@ -21,14 +21,14 @@ output "rate_policy_page_view_requests_id" {
 output "reputation_profile_ids" {
   description = "Map of reputation profile IDs"
   value = var.enable_client_reputation ? {
-    web_attackers_high_threat        = akamai_appsec_reputation_profile.web_attackers_high_threat[0].reputation_profile_id
-    web_attackers_high_threat_shared = akamai_appsec_reputation_profile.web_attackers_high_threat_shared[0].reputation_profile_id
-    web_attackers_low_threat         = akamai_appsec_reputation_profile.web_attackers_low_threat[0].reputation_profile_id
-    web_attackers_low_threat_shared  = akamai_appsec_reputation_profile.web_attackers_low_threat_shared[0].reputation_profile_id
-    dos_attackers_high_threat        = akamai_appsec_reputation_profile.dos_attackers_high_threat[0].reputation_profile_id
-    dos_attackers_high_threat_shared = akamai_appsec_reputation_profile.dos_attackers_high_threat_shared[0].reputation_profile_id
-    dos_attackers_low_threat         = akamai_appsec_reputation_profile.dos_attackers_low_threat[0].reputation_profile_id
-    dos_attackers_low_threat_shared  = akamai_appsec_reputation_profile.dos_attackers_low_threat_shared[0].reputation_profile_id
+    web_attackers_high_threat         = akamai_appsec_reputation_profile.web_attackers_high_threat[0].reputation_profile_id
+    web_attackers_high_threat_shared  = akamai_appsec_reputation_profile.web_attackers_high_threat_shared[0].reputation_profile_id
+    web_attackers_low_threat          = akamai_appsec_reputation_profile.web_attackers_low_threat[0].reputation_profile_id
+    web_attackers_low_threat_shared   = akamai_appsec_reputation_profile.web_attackers_low_threat_shared[0].reputation_profile_id
+    dos_attackers_high_threat         = akamai_appsec_reputation_profile.dos_attackers_high_threat[0].reputation_profile_id
+    dos_attackers_high_threat_shared  = akamai_appsec_reputation_profile.dos_attackers_high_threat_shared[0].reputation_profile_id
+    dos_attackers_low_threat          = akamai_appsec_reputation_profile.dos_attackers_low_threat[0].reputation_profile_id
+    dos_attackers_low_threat_shared   = akamai_appsec_reputation_profile.dos_attackers_low_threat_shared[0].reputation_profile_id
     scanning_tools_high_threat        = akamai_appsec_reputation_profile.scanning_tools_high_threat[0].reputation_profile_id
     scanning_tools_high_threat_shared = akamai_appsec_reputation_profile.scanning_tools_high_threat_shared[0].reputation_profile_id
     scanning_tools_low_threat         = akamai_appsec_reputation_profile.scanning_tools_low_threat[0].reputation_profile_id
